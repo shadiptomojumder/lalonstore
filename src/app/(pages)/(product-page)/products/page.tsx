c301d57251c1cc1c3fab01a6ada1c1d9bcf0ca87
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-03-05 17:48:45 +0600

    fix: adjust grid layout for product cards on product page

--- src/app/(pages)/(product-page)/products/page.tsx
+++ src/app/(pages)/(product-page)/products/page.tsx
@@ -101,7 +101,7 @@ const ProductPage = () => {
                         ))}
                     </div>
                 ) : (
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                         {products?.map((product: Product) => {
                             return <ProductCard key={product?.id} product={product} />;
                         })}
